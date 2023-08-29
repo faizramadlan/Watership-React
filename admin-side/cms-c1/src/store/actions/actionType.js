11dd99cee2a8107f165ceb25fbe1e4ca454c8fc2
Faiz <faiz.ramadlan@is-gs.com>
2023-08-29 07:06:49 +0700

    adding vercel to server

--- admin-side/cms-c1/src/store/actions/actionType.js
+++ admin-side/cms-c1/src/store/actions/actionType.js
@@ -1,4 +1,4 @@
-// export const baseUrl = "http://localhost:3000";
-export const baseUrl = "https://server.oslooow.site";
+export const baseUrl = "http://localhost:3000";
+// export const baseUrl = "https://server.oslooow.site";
 export const PRODUCTS = "products/fetchSuccess";
 export const CATEGORIES = "categories/fetchSuccess";
